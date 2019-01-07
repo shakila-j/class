786e4eb1e9afcfdd32e8aba2812460cdfaf6a09d
Shakila Jahanbin <shakila.jahanbin@yahoo.com>
2019-01-07 16:55:35 +0330

    first

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("hi")
+'use strict';
+
+function highAndLow (str) {
+  let srt = '1 2 3 4 5';
+  console.log(Math.min (str)+ Math.max(str))
+  str.sort (a-b =>a-b);
+  
+
+
+}
---
+++ test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+function highAndLow (str) {
+  let srt = '1 2 3 4 5';
+  for(let char of str){
+  (Math.min (str)+ Math.max(str)
+  return char;
+  char.sort (a-b =>a-b);
+  
+
+
+}
